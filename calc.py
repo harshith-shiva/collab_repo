cdf09dc11958eba5d0975d37967cdfc51610fc2b
harshith-shiva <harshithshivab@gmail.com>
2025-09-17 17:46:45 +0530

    modified baby

--- calc.py
+++ calc.py
@@ -3,8 +3,16 @@ def mul(a,b):
 def div(a,b):
     print(a/b)
 
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a+b)
+
 a=int(input("Enter number 1:"))
 b=int(input("Enter number 2:"))
 
 mul(a,b)
 div(a,b)
+add(a,b)
+sub(a,b)
+#vishwajith 
